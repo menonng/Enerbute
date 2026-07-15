cd3373e214d2254b95841a8b2143567dc82130c7
uracil <sundaeng2010@gmail.com>
2026-07-15 09:40:45 +0900

    Add files via upload

---
+++ conditioner/preprocessing.py
@@ -0,0 +1,8 @@
+from sklearn.preprocessing import *
+from sklearn.model_selection import * 
+import pandas as pd
+import numpy
+
+efl = pd.read_csv(r"C:\Users\user\Documents\파이썬\conditioner\data\data_00.csv")
+print(efl.head())
+
