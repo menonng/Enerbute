17563806f69a7238a47f50c4a0f68953a623ecf2
M_nong <sundaeng2010@gmail.com>
2026-07-15 14:11:53 +0900

    Delete conditioner/preprocessing.py

--- conditioner/preprocessing.py
+++
@@ -1,8 +0,0 @@
-from sklearn.preprocessing import *
-from sklearn.model_selection import * 
-import pandas as pd
-import numpy
-
-efl = pd.read_csv(r"C:\Users\user\Documents\파이썬\conditioner\data\data_00.csv")
-print(efl.head())
-
